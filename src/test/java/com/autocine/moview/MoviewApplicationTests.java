cf36a6dc510c67d2dced6f27a2b86b7ed7bcf840
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-09-03 19:17:37 -0500

    add: agregando proyecto inicial de Spring Boot

---
+++ src/test/java/com/autocine/moview/MoviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autocine.moview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
