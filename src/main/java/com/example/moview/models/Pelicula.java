e42f29f284516c583993f3bba6febff71db1faed
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-09-16 18:31:47 -0500

    fix: se soluciona el problema de atributo 'categoria' faltante

--- src/main/java/com/example/moview/models/Pelicula.java
+++ src/main/java/com/example/moview/models/Pelicula.java
@@ -19,6 +19,7 @@ import lombok.NoArgsConstructor;
         @NamedQuery(name = "Pelicula.findByDuracion", query = "SELECT p FROM Pelicula p WHERE p.duracion = :duracion"),
         @NamedQuery(name = "Pelicula.findByClasificacion", query = "SELECT p FROM Pelicula p WHERE p.clasificacion = :clasificacion"),
         @NamedQuery(name = "Pelicula.findByIdioma", query = "SELECT p FROM Pelicula p WHERE p.idioma = :idioma"),
+        @NamedQuery(name = "Pelicula.findByCategoria", query = "SELECT p FROM Pelicula p WHERE p.categoria = :categoria"),
         @NamedQuery(name = "Pelicula.findByFormato", query = "SELECT p FROM Pelicula p WHERE p.formato = :formato"),
         @NamedQuery(name = "Pelicula.findByTrama", query = "SELECT p FROM Pelicula p WHERE p.trama = :trama"),
         @NamedQuery(name = "Pelicula.findByDirector", query = "SELECT p FROM Pelicula p WHERE p.director = :director")
@@ -44,6 +45,9 @@ public class Pelicula {
 
     @Column( name = "idioma" )
     String idioma;
+
+    @Column( name = "categoria" )
+    String categoria;
 
     @Column( name = "formato" )
     String formato;
