7c147183f368655abd7fb0404284eb7a64c71d0b
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-09-26 18:58:20 -0500

    fix: se arregla la identación de la documentación de MoviewApplication

--- src/main/java/com/example/moview/MoviewApplication.java
+++ src/main/java/com/example/moview/MoviewApplication.java
@@ -18,19 +18,20 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class MoviewApplication {
 
-	/**
-	 * Método principal que inicia la aplicación Moview.
-	 * <p>
-	 * Este método ejecuta el servidor y permite acceder a las funcionalidades (Peliculas).
-	 * </p>
-	 * <p>
-	 * Para revisar el servidor, dirígete al siguiente enlace:
-	 * <a href="http://localhost:8080/moview/peliculas/lista">Moview - Peliculas</a>.
-	 * </p>
-	 *
-	 * @param args Este método inicia la aplicación. No necesitas pasar instrucciones a menos que quieras
-	 *             realizar mejoras de rendimiento.
-	 */
+/**
+ * Método principal que inicia la aplicación Moview.
+ * <p>
+ *     Este método ejecuta el servidor y permite acceder a las funcionalidades (Peliculas).
+ * </p>
+ * <p>
+ *     Para revisar el servidor, dirígete al siguiente enlace:
+ *     <a href="http://localhost:8080/moview/peliculas/lista">Moview - Peliculas</a>.
+ * </p>
+ *
+ * @param args
+ * Este método inicia la aplicación. No necesitas pasar instrucciones a menos que quieras
+ *             realizar mejoras de rendimiento.
+ */
 	public static void main(String[] args) {
 		SpringApplication.run(MoviewApplication.class, args);
 	}
