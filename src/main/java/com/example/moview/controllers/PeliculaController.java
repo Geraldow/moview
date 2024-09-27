0f708f253598d04af7278962329108a25dc8abaa
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-09-26 22:11:28 -0500

    add: se agrega documentación al controlador Pelicula

--- src/main/java/com/example/moview/controllers/PeliculaController.java
+++ src/main/java/com/example/moview/controllers/PeliculaController.java
@@ -10,15 +10,47 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+/**
+ * Esta clase actúa como un controlador en el framework Spring MVC, encargado de manejar
+ * las solicitudes HTTP relacionadas con las películas. Es decir, se encarga de recibir peticiones
+ * del cliente (por ejemplo, desde un navegador web), procesarlas y generar respuestas.
+ *  <br><br>
+ * <strong>@Controller</strong><br>
+ *  Indica que esta clase es un componente de Spring y que se encargará de manejar las solicitudes
+ *  HTTP. Spring escanea las clases anotadas con `@Controller` para encontrar los métodos que
+ *  manejarán las diferentes peticiones (GET, POST, PUT, DELETE, etc.).
+ */
 @Controller
 @RequestMapping("/peliculas")
 public class PeliculaController {
 
+    /**
+     * <strong>@Autowired</strong>
+     * <br>
+     * Inyecta una instancia de la clase PeliculaService en este bean.
+     *
+     * Spring Framework utiliza esta anotación para poder hacer
+     * la inyección de dependencias, conectándose de forma automática
+     * este controlador con el servicio de películas
+     */
     @Autowired
     private PeliculaService peliculaService;
 
+    /**
+     * <strong>@GetMapping</strong><br>
+     * Muestra una lista de todas las películas disponibles.
+     *
+     * <p>Este método recupera todas las películas de la base de datos a través del servicio de películas
+     * y las agrega al modelo para ser renderizadas en la vista. La vista se encarga de presentar la lista
+     * de películas al usuario en un formato legible.
+     *
+     * @param model El modelo utilizado para compartir datos entre el controlador y la vista.
+     *              En este caso, se añade una lista de películas al modelo con la clave "listaPeliculas".
+     * @return El nombre de la plantilla de vista que se utilizará para mostrar la lista de películas.
+     *         En este caso, es "/peliculas/lista-peliculas".
+     */
     // === Path for list movie === //
-    @RequestMapping("/lista")
+    @GetMapping("/lista")
     public String mostrarPeliculas(Model model){
 
         List<Pelicula> listaPeliculas = peliculaService.buscarPeliculas();
@@ -27,6 +59,15 @@ public class PeliculaController {
         return "/peliculas/lista-peliculas";
     }
 
+    /** <strong>@RequestMapping</strong> <br>
+     * Maneja la solicitud para mostrar un formulario para crear una nueva película.
+     *
+     * <p>Este método es responsable de preparar un nuevo objeto {@link Pelicula} y agregarlo al modelo.
+     * Luego, el modelo se utiliza para completar el formulario en la vista.
+     *
+     * @param model El modelo utilizado para compartir datos entre el controlador y la vista.
+     * @return El nombre de la plantilla de vista a representar, que es "/peliculas/nueva-pelicula".
+     */
     // === Path for a new movie === //
     @RequestMapping("/nuevo")
     public String nuevaPelicula( Model model ){
@@ -35,15 +76,37 @@ public class PeliculaController {
         return "/peliculas/nueva-pelicula";
     }
 
+    /**
+     * <strong>@PostMapping</strong> <br>
+     * Crea una nueva película a partir de los datos proporcionados en el formulario.
+     *
+     * <p>Este método recibe un objeto {@link Pelicula} que contiene los datos de la nueva película.
+     * Luego, delega la creación de la película al servicio de películas, el cual se encarga de
+     * persistir los datos en la base de datos. Finalmente, redirige al usuario a la lista de películas.
+     *
+     * @param pelicula La película a crear, obtenida del formulario.
+     * @return La URL a la que se redirige después de crear la película.
+     */
     // === Path for save a movie === //
-    @RequestMapping("/guardar")
+    @PostMapping("/guardar")
     public String crearPelicula(@ModelAttribute("pelicula") Pelicula pelicula){
         peliculaService.crearPelicula(pelicula);
         return "redirect:/peliculas/lista";
     }
 
+    /**
+     * <strong>@PutMapping</strong><br>
+     * Muestra un formulario precargado con los datos de una película existente para editarla.
+     *
+     * <p>Este método busca la película con el ID proporcionado en el servicio de películas.
+     * Luego, crea un objeto {@link ModelAndView} para representar la vista de edición y agrega la película
+     * al modelo para que se muestre en el formulario.
+     *
+     * @param id El identificador de la película a editar.
+     * @return Un objeto `ModelAndView` que contiene el nombre de la vista y los datos de la película.
+     */
     // === Path for update a movie by id === //
-    @RequestMapping("/actualizar/{id}")
+    @PutMapping("/actualizar/{id}")
     public ModelAndView editarPelicula (@PathVariable (name = "id") int id){
         ModelAndView modelAndView = new ModelAndView("/peliculas/editar-pelicula");
         Pelicula pelicula = peliculaService.buscarPeliculaPorId(id);
@@ -51,8 +114,18 @@ public class PeliculaController {
         return modelAndView;
     }
 
+    /**
+     * <strong>@DeleteMapping</strong><br>
+     * Elimina una película por su id.
+     *
+     * <p>Este método delega la eliminación de la película al servicio de películas, el cual se encarga
+     * de borrar los datos de la base de datos. Finalmente, redirige al usuario a la lista de películas.
+     *
+     * @param id El identificador de la película a eliminar.
+     * @return La URL a la que se redirige después de eliminar la película.
+     */
     // === Path for delete a movie by id === //
-    @RequestMapping("eliminar/{id}")
+    @DeleteMapping("eliminar/{id}")
     public String eliminarPelicula(@PathVariable (name = "id") int id){
         peliculaService.eliminarPeliculaPorId(id);
         return "redirect:/peliculas/lista";
