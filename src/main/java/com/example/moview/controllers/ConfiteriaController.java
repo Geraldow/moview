070af81b0254adbf92a5d212f1a8ba20a13183ff
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-10-14 10:37:08 -0500

    refactor(pages): se renombra el controlador de 'PagesController' a 'ConfiteriaController'
    Se renombra el controlador de 'PagesController' a 'ConfiteriaController' para reflejar mejor su propósito y la funcionalidad del controlador.

--- src/main/java/com/example/moview/controllers/ConfiteriaController.java
+++ src/main/java/com/example/moview/controllers/ConfiteriaController.java
@@ -2,18 +2,14 @@ package com.example.moview.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class PagesController {
+@RequestMapping("/confiteria")
+public class ConfiteriaController {
 
-    // Rutas para todas las Páginas de Moview
-    @GetMapping("/movies")
-    public String mostrarMovies() {
-        return "movies/movies";
-    }
-
-    @GetMapping("/confiteria")
+    @GetMapping
     public String mostrarConfiteria() {
         return "confiteria/confiteria";
     }
