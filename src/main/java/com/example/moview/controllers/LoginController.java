f561b267af3f20b6161af5c15a943e728b025a47
Alessandro Marquina <76287712@certus.edu.pe>
2024-10-14 23:19:15 -0500

    Se añaden Controladores para Login y Registro

---
+++ src/main/java/com/example/moview/controllers/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.moview.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    
+    @GetMapping
+    public String mostrarLogin() {
+        return "login/login";
+    }
+}
