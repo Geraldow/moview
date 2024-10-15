e3573f7d125a61ab1ea5337e7b202b8275cc0d75
Alessandro Marquina <76287712@certus.edu.pe>
2024-10-14 19:46:07 -0500

    Prueba de Estructura de carpetas (Cartelera) y confiteria creada <carpeta> essentials

---
+++ src/main/java/com/example/moview/controllers/MoviesController.java
@@ -0,0 +1,16 @@
+package com.example.moview.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/movies")
+
+public class MoviesController {
+
+    @GetMapping
+    public String mostrarMovies() {
+        return "movies/movies";
+    }
+}
