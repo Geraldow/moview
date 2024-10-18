0b496b24b9415a4af2b39e01f98057ce2f3d2680
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-10-17 23:34:34 -0500

    refactor(modularización): Se renombra la clase RegisterController
    Se renombra la clase `RegisterController` a `net.moview.api.ecommerce.auth.RegisterController` para reflejar la estructura modular del proyecto.

--- controller/src/main/java/net/moview/api/ecommerce/auth/RegisterController.java
+++ controller/src/main/java/net/moview/api/ecommerce/auth/RegisterController.java
@@ -1,4 +1,4 @@
-package com.example.moview.controllers;
+package net.moview.api.ecommerce.auth;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
